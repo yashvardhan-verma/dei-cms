8cca4b9236bf9c4c4c8ea51bc0f574d3d13ca7ac
Yashvardhan <iamyashvardhan6@gmail.com>
2021-02-17 17:43:25 +0530

    Added login feature.

---
+++ php/login.php
@@ -0,0 +1,35 @@
+<?php
+ob_start();
+session_start();
+
+require ('../config/config.php');
+
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if(!$con){
+       die("connection to this DB failed". mysqli_connect_error());
+   }
+
+$roll_no = mysqli_real_escape_string($con, $_POST['username']);
+$dob = mysqli_real_escape_string($con, $_POST['password']);
+
+
+if (DB_SERVER == 'localhost')
+{
+    $sql = "SELECT roll_no, dob FROM development.login_cred WHERE roll_no = '$roll_no' and dob = '$dob'";
+
+}else{
+    $sql = "SELECT roll_no, dob FROM Rc1TptrQqu.login_cred WHERE roll_no = '$roll_no' and dob = '$dob'";
+
+}
+
+$result = $con->query($sql);
+
+if($result->num_rows > 0)
+{
+    $_SESSION['timeout'] = time();
+    $_SESSION['username'] = $roll_no;
+    header('Location: ../home.php');
+} else {
+    echo "Not Registered";
+}
